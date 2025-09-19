82817220f9e08d0546bb0b1a437f3671da890057
KseniyaD0 <kseniyadudik@gmail.com>
2025-09-19 14:13:04 +0300

    Update prob2.cs
    операции search теперь возвращает название белка или "NOT FOUND", diff - кол-во отличий в цепочках аминокислот. Операция mode возвращает кортеж, где item1 - чаще-встречающаяся буква, item2 - кол-во буквы в цепочке

--- Lab1/prob2.cs
+++ Lab1/prob2.cs
@@ -11,7 +11,6 @@ struct GeneticData
 
 
 
-
 class PROB2
 {
     static List<GeneticData> geneticData = new List<GeneticData>();
@@ -20,21 +19,34 @@ class PROB2
     {
         AddGeneticData();
 
-        Console.WriteLine(" SEARCH: ");  //поиск куска цепочки среди всех цепочек
-        Search("SIIK");
-        Search("PLML");
-        Search("FK3I");
+//поиск куска цепочки среди всех цепочек
+        Console.WriteLine(" SEARCH: ");  
+        Console.WriteLine("SIIK :" + Search("SIIK"));
+        Console.WriteLine("PLML :" + Search("PLML"));
+        Console.WriteLine("FK3I :" + Search("FK3I"));
 
 
-        Console.WriteLine("\nDIFF:");  //во сколько символов отличаются две цепочки
-        Diff("6.8 kDa mitochondrial proteolipid", "Alcohol dehydrogenase");
-        Diff("RNA-dependent RNA polymerase [Fragment]", "Alcohol dehydrogenase");
-        Diff("Cecropin", "Pre-T/NK cell associated protein 6H9A");
+//во сколько символов отличаются две цепочки
+        Console.WriteLine("\nDIFF:");  
+
+        Console.WriteLine("Белки: 6.8 kDa mitochondrial proteolipid, Alcohol dehydrogenase");
+        Console.WriteLine("Цепочки отличаются в " +  Diff("6.8 kDa mitochondrial proteolipid", "Alcohol dehydrogenase"));
+
+        Console.WriteLine("Белки: RNA-dependent RNA polymerase [Fragment], Alcohol dehydrogenase");
+        Console.WriteLine("Цепочки отличаются в " +  Diff("RNA-dependent RNA polymerase [Fragment]", "Alcohol dehydrogenase"));
 
 
-        Console.WriteLine("\nMODE:"); //определить самый чаще-встречающийся символ в цепочке (декодированный варик)
-        Mode("Cecropin");
-        Mode("Alcohol dehydrogenase");
+        Console.WriteLine("Белки: Cecropin, Pre-T/NK cell associated protein 6H9A");
+        Console.WriteLine("Цепочки отличаются в " +  Diff("Cecropin", "Pre-T/NK cell associated protein 6H9A"));  
+
+
+
+//определить самый чаще-встречающийся символ в цепочке (декодированный варик)
+        Console.WriteLine("\nMODE:");
+        var result = Mode("Cecropin");
+        Console.WriteLine("В Cecropin: " + result.Item1 + ", " + result.Item2);
+        result = Mode("Alcohol dehydrogenase");
+        Console.WriteLine("В Alcohol dehydrogenase: " + result.Item1 + ", " + result.Item2);
     }
 
 
@@ -79,44 +91,30 @@ class PROB2
 
 
 
-    static void Search(string proteinPart)
+static string Search(string proteinPart)
+{
+    // декодировать кусок цепочки
+    string decoded = Decode(proteinPart);
+    
+    foreach (var data in geneticData)
     {
-        //декодировать кусок цепочки
-        string decoded = Decode(proteinPart); 
-        bool found = false;
-
-        foreach (var k in geneticData)
+        if (data.amino_acids.Contains(decoded))
         {
-
-            if (k.amino_acids.Contains(decoded))
-            {
-
-                Console.WriteLine("\nБелок: " + k.protein);
-                Console.WriteLine("Организм: " + k.organism);
-                Console.WriteLine("Фрагмент: " + proteinPart);
-                found = true;
-            }
-        }
-
-        if (!found) //если не нашло
-        {
-            Console.WriteLine("\n" + proteinPart + " NOT FOUND");
+            return data.protein; // Возвращаем название белка
         }
     }
+    return "NOT FOUND";
+}
 
 
-
-
-
- static void Diff(string p1, string p2)
+ static int Diff(string p1, string p2)
     {
     GeneticData protein1 = geneticData.FirstOrDefault(p => p.protein == p1);
     GeneticData protein2 = geneticData.FirstOrDefault(p => p.protein == p2);
 
-        int minLength = Math.Min(protein1.amino_acids.Length, protein2.amino_acids.Length);
         int differences = 0;
 
-        for (int i = 0; i < minLength; i++)
+        for (int i = 0; i < Math.Min(protein1.amino_acids.Length, protein2.amino_acids.Length); i++)
         {
             if (protein1.amino_acids[i] != protein2.amino_acids[i])
             {
@@ -125,109 +123,45 @@ class PROB2
         }
 
         differences += Math.Abs(protein1.amino_acids.Length - protein2.amino_acids.Length);
-
-        Console.WriteLine("\nРазличия между "  + p1 + " и " + p2 +" " + differences + " аминокислот");
+        return differences;
     }
 
 
 
-
-
-
-
-    static void Mode(string proteinName)
+    static (char, int) Mode(string proteinName)
     {
 
         GeneticData protein = geneticData.FirstOrDefault(p => p.protein == proteinName);
 
         //словарь где ключ - буква, значение - кол-во этой буквы в цепочке
         Dictionary<char, int> acids = new Dictionary<char, int>();
-        foreach (char aminoAcid in protein.amino_acids)
+        foreach (char a in protein.amino_acids)
         {
-            if (acids.ContainsKey(aminoAcid)) //ключ по букве
+            if (acids.ContainsKey(a)) //ключ по букве
             {
-                acids[aminoAcid]++;
+                acids[a]++;
             }
             else
             {
-                acids[aminoAcid] = 1;
+                acids[a] = 1;
             }
         }
 
         char mostOften = ' '; //буква
         int maxCount = 0;
 
-      foreach (var c in acids) //перебрать весь словарь
-    {
-        if (c.Value > maxCount)
+        foreach (var c in acids) //перебрать весь словарь
         {
-            mostOften = c.Key;  //буква
-            maxCount = c.Value;  //кол-во этой буквы
-        }
-    }
-
-
-        Console.WriteLine("\nБелок:" + protein.protein);
-        Console.WriteLine("Организм: " + protein.organism);
-        Console.WriteLine("Наиболее частая аминокислота: " + mostOften + ", " + maxCount);
-
-    }
-
-
-
-  /* не понадобилось в коде с листом GeneticData
-  
-   static bool IsValidString(string input)
-    {
-
-        //только заглавные латинские буквы
-        bool matcher = Regex.IsMatch(input, @"^[A-Z]+$");
-        return matcher;
-    }
-
-
-    static string Encode(string stroka)
-    {
-        StringBuilder result = new StringBuilder();
-        int count = 1;
-        char currentChar = stroka[0];
-
-        for (int i = 1; i < stroka.Length; i++)
-        {
-            if (stroka[i] == currentChar)
+            if (c.Value > maxCount)
             {
-                count++;
-            }
-            else
-            {
-
-                if (count > 2)
-                {
-                    result.Append(count).Append(currentChar);
-                }
-                else
-                {
-                    result.Append(new string(currentChar, count));
-                }
-
-                currentChar = stroka[i];
-                count = 1;
+                mostOften = c.Key;  //буква
+                maxCount = c.Value;  //кол-во этой буквы
             }
         }
 
-        // последний символ
-        if (count > 2)
-        {
-            result.Append(count).Append(currentChar);
-        }
-        else
-        {
-            result.Append(new string(currentChar, count));
-        }
-
-        return result.ToString();
+        (char, int) result = (mostOften, maxCount);
+        return result; 
     }
-*/
 
 
     static string Decode(string stroka)
