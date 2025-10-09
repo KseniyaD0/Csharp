052307a37ae092c13d6966c3846645452f2074d2
KseniyaD0 <kseniyadudik@gmail.com>
2025-10-09 14:41:22 +0300

    Update Game.cs
    добавлен метод для чтения из файла

--- Lab2/Game.cs
+++ Lab2/Game.cs
@@ -4,7 +4,8 @@ enum GameState
     End
 }
 
-       
+
+
 class Game
 {
     public int size;
@@ -21,11 +22,61 @@ class Game
     }
 
 
+
+
+
+    public (char[] commands, int[] steps) ReadGameDataFromFile(string filename)
+    {
+        List<char> commandList = new List<char>();
+        List<int> stepList = new List<int>();
+
+
+        string[] lines = File.ReadAllLines(filename);
+
+
+
+        for (int i = 1; i < lines.Length; i++)
+        {
+            string line = lines[i].Trim();
+            string[] parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+
+
+            if (parts.Length == 2)  // 'C' и 'M'
+            {
+                string a = parts[0].Trim();
+                char command = Convert.ToChar(a);
+                commandList.Add(command);
+
+
+                string b = parts[1].Trim();
+                int step = Convert.ToInt32(b);
+                stepList.Add(step);
+
+            }
+
+            else if (parts.Length == 1) // 'P'
+            {
+                string a = parts[0].Trim();
+                char command = Convert.ToChar(a);
+                int step = 0;
+                commandList.Add(command);
+                stepList.Add(step);
+
+            }
+        }
+        return (commandList.ToArray(), stepList.ToArray());
+    }
+
+
+
+
+
+
     public void Run()
     {
-        char[] commands = { 'M', 'M', 'P', 'C', 'M', 'P', 'C', 'P', 'M', 'M', 'C', 'P', 'M', 'M', 'P', 'C', 'P', 'C', 'C', 'P', 'M', 'P' };
-        int[] steps = { 7, -5, 0, 6, -7, 0, 6, 0, 4, 6, 0, 0, 0, 6, 0, -1, 0, 1, 4, 0, -4, 0 };
 
+        var (commands, steps) = ReadGameDataFromFile("1.ChaseData.txt");
 
         mouse.location = 1;
         cat.location = 1;
@@ -59,7 +110,6 @@ class Game
                 DoMoveCommand(commands[turn], steps[turn]);
             }
 
-
             if (CheckCatch(mouse, cat))
             {
                 mouse.state = State.Looser;
@@ -79,10 +129,15 @@ class Game
             Console.WriteLine("Мышь убежала! Игра окончена.");
         }
 
-
         Console.WriteLine("Финальные позиции -- Мышь:" + mouse.location + ", Кот:" + cat.location + "\n");
+        Console.WriteLine("Дистанции -- Мышь:" + mouse.distance + ", Кот:" + cat.distance + "\n");
         Console.WriteLine("Победитель: " + (mouse.state == State.Winner ? "Мышь" : "Кот"));
     }
+
+
+
+
+
 
     private void DoMoveCommand(char command, int step)
     {
@@ -91,7 +146,6 @@ class Game
             case 'M':
                 if (mouse.state == State.NotInGame)
                 {
-      
                     mouse.state = State.Playing;
                     Console.WriteLine("Мышь входит в игру!");
                 }
@@ -103,7 +157,6 @@ class Game
             case 'C':
                 if (cat.state == State.NotInGame)
                 {
-
                     cat.state = State.Playing;
                     Console.WriteLine("Кот входит в игру!");
                 }
@@ -114,6 +167,8 @@ class Game
                 break;
         }
     }
+
+
 
     public int CalculateDistance(Player cat, Player mouse)
     {
