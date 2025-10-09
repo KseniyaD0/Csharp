4b8457ce2830700b5c8eb8d7740479820d54facc
KseniyaD0 <kseniyadudik@gmail.com>
2025-10-09 14:42:14 +0300

    Update Player.cs
    добавлены строки для счета всей пройденной дистанции за игру

--- Lab2/Player.cs
+++ Lab2/Player.cs
@@ -10,12 +10,18 @@ class Player
 {
     public string name;
     public int location;
+    public int distance;
     public State state = State.NotInGame;
+
+
+
+
 
     public Player(string name)
     {
         this.name = name;
         this.location = 1;
+        this.distance = 0;
     }
 
     public void Move(int steps, int fieldSize)
@@ -27,12 +33,12 @@ class Player
             Console.WriteLine(name + " входит в игру");
         }
 
-      int newPosition = (location + steps - 1) % fieldSize + 1;
+        int newPosition = (location + steps - 1) % fieldSize + 1;
         location = newPosition;
+        int updateDistance = distance + Math.Abs(steps);
+        distance = updateDistance;
     }
 }
-
-
 
 
 
