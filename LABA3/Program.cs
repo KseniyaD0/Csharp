c2c6f0edce76418037d9154b155be6cc4daf8a62
KseniyaD0 <kseniyadudik@gmail.com>
2025-10-15 16:22:21 +0300

    Update Program.cs

--- LABA3/Program.cs
+++ LABA3/Program.cs
@@ -10,8 +10,21 @@ class Program
         Realization realization = new Realization();
         Text text = realization.Tokenization(TextFromFile);
 
-        Console.WriteLine("Всего предложений: " + text.Sentences.Count);
+
+        Console.WriteLine("Всего предложений: " + text.Sentences.Count + "\n");
+    
+
+
+      for (int i = 0; i < text.Sentences.Count; i++)
+        {
+            Console.WriteLine("Предложение " + (i + 1) + " :");
+            Console.WriteLine("Полное предложение: " + text.Sentences[i] );
+            Console.WriteLine("Количество слов: " + text.Sentences[i].WordCount());
+            Console.WriteLine("Длина предложения: " + text.Sentences[i].Length());
+            Console.WriteLine();
+        }
 
     }
 }
+
 
