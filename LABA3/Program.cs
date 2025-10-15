735d16bde2eb8bd2ea398a17687c043b299c3bd8
KseniyaD0 <kseniyadudik@gmail.com>
2025-10-15 16:05:02 +0300

    Update Program.cs

--- LABA3/Program.cs
+++ LABA3/Program.cs
@@ -1,11 +1,17 @@
-﻿using System;
+using System;
 
 class Program
 {
     static void Main(string[] args)
     {
+        string TextFromFile = " - Что такое переменная. Прежде чем перейти к рассмотрению потребности в" +
+                             " использовании переменных в языке программирования, сделаем небольшое отступление" +
+                             " и рассмотрим, как компьютер хранит и обрабатывает данные.";
         Realization realization = new Realization();
+        Text text = realization.Tokenization(TextFromFile);
+
+        Console.WriteLine("Всего предложений: " + text.Sentences.Count);
+
     }
 }
-
 
