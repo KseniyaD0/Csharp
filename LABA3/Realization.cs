c22f5aa512ccba13b9d2049b093cf075662b2e7f
KseniyaD0 <kseniyadudik@gmail.com>
2025-10-15 16:07:35 +0300

    Update Realization.cs
    добавлен метод токенизации (разбиение текста на список предложений, предложения - список слов и пунктуации)

--- LABA3/Realization.cs
+++ LABA3/Realization.cs
@@ -2,14 +2,72 @@ using System.Text;
 
 class Realization
 {
-        
-        
 
 
 
-    public void Tokenization()
+    public Text Tokenization(string TextFromFile)
     {
-  
+        Text text = new Text();
+        Sentence sentence = new Sentence();
+        StringBuilder word = new StringBuilder();
+
+        for (int i = 0; i < TextFromFile.Length; i++)
+        {
+            char c = TextFromFile[i];
+
+            bool NOWInWord = false;
+
+            //буква
+            if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') ||
+                (c >= 'а' && c <= 'я') || (c >= 'А' && c <= 'Я'))
+            {
+                word.Append(c);
+                NOWInWord = true;
+            }
+
+
+
+            //если пробел
+            else if (char.IsWhiteSpace(c))
+            {
+                if (NOWInWord)
+                {
+                    sentence.AddWord(new Word(word.ToString()));
+                    word.Clear();
+                    NOWInWord = false;
+                }
+            }
+
+
+
+            //пунктуация
+            else if ((c == ',') || (c == ';') || (c == ':') ||
+                    (c == '-') || (c == '(') || (c == ')') || (c == '"'))
+            {
+                if (NOWInWord)
+                {
+                    sentence.AddWord(new Word(word.ToString()));
+                    word.Clear();
+                    NOWInWord = false;
+                }
+
+                sentence.AddPunctuation(new Punctuation(c));
+            }
+
+            else if ((c == '.') || (c == '!') || (c == '?'))
+            {
+                text.AddSentence(sentence);
+                sentence = new Sentence();
+            }
+
+        }
+
+        if (sentence.Elements.Count > 0)
+        {
+            text.AddSentence(sentence);
+        }
+
+        return text;
     }
 
 }
