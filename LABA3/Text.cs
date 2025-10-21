3bab6d2440fbda85046baf613bc1e1c454e707e7
KseniyaD0 <kseniyadudik@gmail.com>
2025-10-21 13:45:38 +0300

    Update Text.cs

--- LABA3/Text.cs
+++ LABA3/Text.cs
@@ -1,24 +1,47 @@
 using System.Text;
 
-public class Text
-    {
-        public List<Sentence> Sentences = new List<Sentence>();
+public class Text : IText
+{
+    public List<Sentence> Sentences = new List<Sentence>();
 
     public void AddSentence(Sentence sentence)
     {
         Sentences.Add(sentence);
     }
-        
-        
 
 
-        public override string ToString()
+    public override string ToString()
+    {
+        var text = new StringBuilder();
+        foreach (var sentence in Sentences)
         {
-            var text = new StringBuilder();
-            foreach (var sentence in Sentences)
-            {
-                text.AppendLine(sentence.ToString());
-            }
-            return text.ToString();
+            text.AppendLine(sentence.ToString());
         }
+        return text.ToString();
     }
+
+
+    public int Length()
+    {
+        int length = 0;
+        foreach (var sentence in Sentences)
+        {
+            length += sentence.Length();
+        }
+        return length;
+    }
+
+
+
+    public int WordCount()
+    {
+        int count = 0;
+        foreach (var sentence in Sentences)
+        {
+            count += sentence.WordCount();
+        }
+        return count;
+    }
+
+}
+
